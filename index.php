425e2ac20e5950764b83e625b0b350af5e72c4b7
kristijanvasiljkovski <kristijan.vasiljkovski.00@gmail.com>
2021-04-06 17:28:45 +0200

    Homework19

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$one=50;
+$two=60;
+$three=70;
+$four=80;
+
+echo $two-$one;
+echo $three-$two;
+
+$result=$two-$one;
+echo $two-$one=$result;
+$result=$four-$three;
+echo $four-$three=$result;
